4daaf619d765329a675f44381652cd28c4eb10a9
PolinaKoval <polina.koval.32@gmail.com>
2016-03-04 21:24:35 +0500

    обработка where.where и set.set

--- multivarka.js
+++ multivarka.js
@@ -1,113 +1,99 @@
 'use strict';
 const MongoClient = require('mongodb').MongoClient;
 
-function doQuery(objectForQuery, callback) {
-    MongoClient.connect(objectForQuery.url, function (err, db) {
+module.exports.server = url => ({
+    collection: colName => new ObjectForQuery(url, colName)
+});
+
+const MongoFunc = {
+    find: function (innerCallback) {
+        this.collection.find(correctQuery(this.query)).toArray(innerCallback);
+    },
+    remove: function (innerCallback) {
+        this.collection.deleteMany(correctQuery(this.query), innerCallback);
+    },
+    update: function (innerCallback) {
+        this.collection.updateMany(correctQuery(this.query),
+            correctQuery(this.setQuery, '$set'), innerCallback);
+    },
+    insert: function (innerCallback) {
+        this.collection.insertOne(this.insertObj, innerCallback);
+    }
+};
+
+function correctQuery(query, key) {
+    key = key || '$and';
+    return Object.keys(query).length ? {[key]: query} : {};
+}
+
+const operations = {
+    find: function (callback) {
+        this.doQuery('find', callback);
+    },
+    remove: function (callback) {
+        this.doQuery('remove', callback);
+    },
+    update: function (callback) {
+        this.doQuery('update', callback);
+    },
+    insert: function (newObj, callback) {
+        this.doQuery('insert', callback, newObj);
+    },
+    where: function (field) {
+        return new ParamsSetter(this, field ? [field] : []);
+    },
+    set: function (newField, value) {
+        if (arguments.length == 2) {
+            this.setQuery[newField] = value;
+        }
+        return this;
+    }
+};
+
+let ParamsSetter = function (queryObj, fields, not) {
+    let index = not ? 1 : 0;
+    this.not = () => new ParamsSetter(queryObj, fields, true);
+    this.equal = value => this.addParam(value, ['$eq', '$ne'][index]);
+    this.lessThan = num => this.addParam(num, ['$lt', '$gte'][index]);
+    this.greatThan = num => this.addParam(num, ['$gt', '$lte'][index]);
+    this.include = value => this.addParam(value, ['$in', '$nin'][index]);
+    this.where = field => {
+        fields.push(field);
+        return new ParamsSetter(queryObj, fields, not);
+    };
+    this.addParam = (value, param) => {
+        let newQuery = {[param]: value};
+        let queries = fields.map(field => ({[field]: newQuery}));
+        [].push.apply(queryObj.query, queries);
+        return queryObj;
+    };
+};
+
+let ObjectForQuery = function (url, collectionName) {
+    this.url = url;
+    this.collectionName = collectionName;
+    this.query = [];
+    this.setQuery = {};
+    this.queryFunc = null;
+    this.doQuery = doQuery;
+    Object.setPrototypeOf(this, operations);
+};
+
+function doQuery(operation, callback, insertObj) {
+    this.insertObj = insertObj || null;
+    this.queryFunc = MongoFunc[operation];
+    connectMongo.call(this, callback);
+};
+
+function connectMongo(callback) {
+    MongoClient.connect.call(this, this.url, (err, db) => {
         if (err) {
             return console.log(err);
         }
-        let collection = db.collection(objectForQuery.collectionName);
-        objectForQuery.queryFunction(collection, function (err, result) {
+        this.collection = db.collection(this.collectionName);
+        this.queryFunc(function (err, result) {
             callback(err, result);
             db.close();
         });
     });
 }
-
-function createOperation(objectForQuery) {
-    let operations = {
-        find: function (callback) {
-            let func = function (collection, innerCallback) {
-                collection.find(objectForQuery.query).toArray(innerCallback);
-            };
-            objectForQuery.queryFunction = func;
-            doQuery(objectForQuery, callback);
-        },
-        remove: function (callback) {
-            let func = function (collection, innerCallback) {
-                collection.deleteMany(objectForQuery.query, innerCallback);
-            };
-            objectForQuery.queryFunction = func;
-            doQuery(objectForQuery, callback);
-        },
-        insert: function (newObject, callback) {
-            let func = function (collection, innerCallback) {
-                collection.insertOne(newObject, innerCallback);
-            };
-            objectForQuery.queryFunction = func;
-            doQuery(objectForQuery, callback);
-        },
-        set: function (newField, value) {
-            let innerQuery = {};
-            innerQuery[newField] = value;
-            let setQuery = {$set: innerQuery};
-            _this = this;
-            return {
-                update: function (callback) {
-                    let func = function (collection, innerCallback) {
-                        collection.updateMany(objectForQuery.query, setQuery, innerCallback);
-                    };
-                    objectForQuery.queryFunction = func;
-                    doQuery(objectForQuery, callback);
-                }
-            };
-        },
-        where: function (field) {
-            return createQuery(objectForQuery, field);
-        }
-    };
-    return operations;
-}
-
-function createQuery(objectForQuery, field, not) {
-    let queryParams = {
-        not: function () {
-            return createQuery(objectForQuery, field, true);
-        },
-        equal: function (param) {
-            let query = not ? { $ne: param } : param;
-            return this.addQuery(query);
-        },
-        lessThan: function (num) {
-            let query = not ? { $gte: num } : { $lt: num };
-            return this.addQuery(query);
-        },
-        greatThan: function (num) {
-            let query = not ? { $lte: num } : { $gt: num };
-            return this.addQuery(query);
-        },
-        include: function (params) {
-            let query = not ? { $nin: params } : { $in: params };
-            return this.addQuery(query);
-        },
-        addQuery: function (newQuery) {
-            let query = {};
-            query[field] = newQuery;
-            objectForQuery.query['$and'].push(query);
-            return createOperation(objectForQuery);
-        }
-    };
-    return queryParams;
-}
-
-let ObjectForQuery = function (url, collectionName) {
-    this.url = url;
-    this.collectionName = collectionName;
-    this.where = function (field) {
-        return createQuery(this, field);
-    };
-    this.insert = function (newObject, callback) {
-        createOperation(this).insert(newObject, callback);
-    };
-    this.query = {$and:[]};
-    this.queryFunction = null;
-};
-
-module.exports.server = function (url) {
-    let collection = {
-        collection: function (collectionName) {
-            return new ObjectForQuery(url, collectionName);
-        }
-    };
-    return collection;
-};
